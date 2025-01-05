7454cdd6284a757641d0dac30cc8ad5596e9fd8c
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-01-05 17:09:58 -0600

    initial remote setup

---
+++ Daisies/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Daisies
+//
+//  Created by Nathaniel Bedggood on 05/01/2025.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("This is a map")
+    }
+}
+
+#Preview {
+    MapView()
+}
