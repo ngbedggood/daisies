dff027f802694e335b9247821eafb9470332edba
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-01-05 16:45:40 -0600

    Initial Commit

---
+++ Daisies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Daisies
+//
+//  Created by Nathaniel Bedggood on 05/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Daisies/DaisiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaisiesApp.swift
+//  Daisies
+//
+//  Created by Nathaniel Bedggood on 05/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct DaisiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
