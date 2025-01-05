5faf8642915548d60b3bbb76cbe5eaf158a49738
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-01-05 16:45:40 -0600

    Initial Commit

---
+++ DaisiesTests/DaisiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  DaisiesTests.swift
+//  DaisiesTests
+//
+//  Created by Nathaniel Bedggood on 05/01/2025.
+//
+
+import Testing
+@testable import Daisies
+
+struct DaisiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
